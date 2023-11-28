37f2d99eddd7ec0d0d9f3f2181e08363e3f5001e
zhung1oz7 <wl00494849@gmail.com>
2023-11-28 09:44:31 +0800

    update

--- w2v.py
+++ w2v.py
@@ -1,22 +1,41 @@
 from gensim.models.word2vec import KeyedVectors
 
-def Main():
-    model = KeyedVectors.load_word2vec_format("/Users/zhung/py/meeting/tmunlp_1.6B_WB_300dim_2020v1.bin.gz",
+model = KeyedVectors.load_word2vec_format("/Users/zhung/py/meeting/tmunlp_1.6B_WB_300dim_2020v1.bin.gz",
                                                             unicode_errors='ignore', 
                                                             binary=True)
 
-    print(model["人口"])
-    print(len(model["人口"]))
-    ## Cosine similarity Value 1 to -1 heighest value more similarity
-    print("人口、人數")
-    print(model.similarity("人口","人數"))
+def help():
+    print("press h call help")
+    print("press q quit process")
 
-    print("人口相似詞")
-    for w, sim in model.most_similar('人口', topn=5):
+def similar (s1:str,s2:str):
+    ## Cosine similarity Value 1 to -1 heighest value more similarity
+    print(f"{s1},{s2} Cosine similarity Value")
+    print(model.similarity(s1,s2))
+
+def most_similar(s:str,top):
+    print(f"{s} 相似詞")
+    for w, sim in model.most_similar(s,topn=top):
         print((w,sim))
-    
-    print("地址相似詞")
-    for w, sim in model.most_similar('地址',topn=3):
-        print((w,sim))
-    
-Main()
+
+def main():
+    help()
+    while 1:
+        cmd = str(input())
+        match cmd:
+            case "s":
+                print("Input similar word")
+                arg = input()
+                similar(arg)
+            case "m":
+                print("Input similar word to List")
+                arg = input()
+                arr = arg.split(" ")
+                most_similar(arr[0],arr[1])
+            case "q":
+                break
+            case _:
+                print("You have some err")
+                help()
+
+main()
